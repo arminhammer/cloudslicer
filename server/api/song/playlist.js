76560e808490430d1c2a51d0c1a4a649a5612910
arminhammer <arminhammer@gmail.com>
2014-11-09 21:16:29 -0500

    Adding more functionality to the playlist closure.

--- server/api/song/playlist.js
+++ server/api/song/playlist.js
@@ -5,6 +5,7 @@ var Playlist = function() {
 
   var tracks = [];
   var manager = null;
+  var currentSong = null;
 
   console.log('Playlist length: %d', tracks.length);
 
@@ -12,40 +13,48 @@ var Playlist = function() {
 
     console.log('Managing');
     console.log('Playlist length: %d', tracks.length);
-    if(tracks.length > 0) {
+    if(currentSong == null) {
 
-      if(tracks[0].tLength > 1000) {
-        tracks[0].tLength -= 1000;
-        console.log('Track length: %d', tracks[0].tLength);
-      } else if(tracks[0].tLength > 0) {
-        tracks[0].tLength = 0;
-        console.log('Track length: %d', tracks[0].tLength);
+      currentSong = {};
+      currentSong.track = tracks.shift();
+      currentSong.position = 0;
+      console.log('Now playing %s', currentSong.track.title);
+    }
+    else {
+
+      if (currentSong.position <= currentSong.track.tLength) {
+        currentSong.position += 1000;
+        console.log('%s now at %d of %d', currentSong.track.title, currentSong.position, currentSong.track.tLength);
       }
-      else if(tracks[0].tLength === 0){
-        console.log('Track finished.');
-        tracks.shift();
+      else {
+        currentSong = null;
       }
 
     }
+
+  };
+
+  // TODO
+  this.refill = function() {
 
   }
 
   this.addTrack = function(track) {
     tracks.push(track);
     console.log('track %s added to the playlist', track);
-  }
+  };
 
   this.getTrackList = function() {
 
     return tracks;
 
-  }
+  };
 
   this.start = function() {
 
     manager = setInterval(manage, 1000);
 
-  }
+  };
 
   this.stop = function() {
 
@@ -54,6 +63,6 @@ var Playlist = function() {
 
   }
 
-}
+};
 
 module.exports = Playlist;
