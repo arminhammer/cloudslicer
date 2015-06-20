5c8f1d817b44d3d5a2b918992cc44766583fa0f8
arminhammer <arminhammer@gmail.com>
2015-06-20 17:11:29 -0400

    Added video list sort by score.

--- client/app/models/Video.js
+++ client/app/models/Video.js
@@ -4,6 +4,7 @@ var Video = function(data) {
   this.title = m.prop(data.title);
   this.description = m.prop(data.description);
   this.youtubeID = m.prop(data.youtubeID);
+  this.score = m.prop(data.score);
 };
 
 module.exports = Video;
--- client/app/views/videoView.js
+++ client/app/views/videoView.js
@@ -4,23 +4,25 @@ var vm = require('../viewModels/videoViewModel');
 vm.init();
 
 var videoView = function() {
-    return m("html", [
-        m("body", [
-            m("input", {onchange: m.withAttr("value", vm.description), value: vm.description()}),
-            m("button", {onclick: vm.add}, "Add"),
-            m("table", [
-                vm.list.map(function(task, index) {
-                    return m("tr", [
-                        m("td", "Task")
-                        //m("td", [
-                         //   m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), checked: task.done()})
-                        //]),
-                        //m("td", {style: {textDecoration: task.done() ? "line-through" : "none"}}, task.description()),
-                    ])
-                })
-            ])
-        ])
-    ]);
+  return m("html", [
+    m("body", [
+      m("input", {onchange: m.withAttr("value", vm.description), value: vm.description()}),
+      m("button", {onclick: vm.add}, "Add"),
+      m("table", [
+        vm.list.map(function(video, index) {
+          return m("tr", [
+            m("td", video.title()),
+            m("td", video.description()),
+            m("td", video.score())
+            //m("td", [
+            //   m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), checked: task.done()})
+            //]),
+            //m("td", {style: {textDecoration: task.done() ? "line-through" : "none"}}, task.description()),
+          ])
+        })
+      ])
+    ])
+  ]);
 };
 
 module.exports = videoView;
