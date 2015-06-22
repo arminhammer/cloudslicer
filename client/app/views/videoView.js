a20cc9a89d5358fb5ec98150fa7486dd0521df54
arminhammer <arminhammer@gmail.com>
2015-06-21 20:23:14 -0400

    Added videojs element to the page

--- client/app/views/videoView.js
+++ client/app/views/videoView.js
@@ -4,25 +4,35 @@ var vm = require('../viewModels/videoViewModel');
 vm.init();
 
 var videoView = function() {
-  return m("html", [
-    m("body", [
-      m("input", {onchange: m.withAttr("value", vm.description), value: vm.description()}),
-      m("button", {onclick: vm.add}, "Add"),
-      m("table", [
-        vm.list.map(function(video, index) {
-          return m("tr", [
-            m("td", video.title()),
-            m("td", video.description()),
-            m("td", video.score())
-            //m("td", [
-            //   m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), checked: task.done()})
-            //]),
-            //m("td", {style: {textDecoration: task.done() ? "line-through" : "none"}}, task.description()),
-          ])
-        })
-      ])
+  return m("div", [
+    m("input", {onchange: m.withAttr("value", vm.description), value: vm.description()}),
+    m("button", {onclick: vm.add}, "Add"),
+    m("table", [
+      vm.list.map(function(video, index) {
+        return m("tr", [
+          m("td", video.title()),
+          m("td", video.description()),
+          m("td", video.score())
+          //m("td", [
+          //   m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), checked: task.done()})
+          //]),
+          //m("td", {style: {textDecoration: task.done() ? "line-through" : "none"}}, task.description()),
+        ])
+      })
+    ]),
+  ]),
+    m("table", [
+      m("video[controls]", {
+        id: "vid1",
+        //src: ""
+        class: "video-js vjs-default-skin",
+        preload:"auto",
+        width: "640",
+        height: "360"
+        //"data-setup": '{"techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v=QcIy9NiNbmo"}'
+      })
     ])
-  ]);
 };
 
 module.exports = videoView;
+
