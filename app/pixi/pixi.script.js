5c298ad7b54830df14ab109845e7b5d7a6398b86
arminhammer <arminhammer@gmail.com>
2015-08-26 22:57:30 -0400

    Added some resizing logic.

--- app/pixi/pixi.script.js
+++ app/pixi/pixi.script.js
@@ -4,8 +4,28 @@
 
 var width = window.innerWidth;
 var height = window.innerHeight;
-var renderer = PIXI.autoDetectRenderer(width, height,{backgroundColor : 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(width, height,{backgroundColor : 0xffffff});
 document.body.appendChild(renderer.view);
+
+// create the root of the scene graph
+var stage = new PIXI.Container();
+
+var container = new PIXI.Container();
+
+stage.addChild(container);
+
+for (var j = 0; j < 5; j++) {
+
+  for (var i = 0; i < 5; i++) {
+    var bunny = PIXI.Sprite.fromImage('_assets/basics/bunny.png');
+    bunny.x = 40 * i;
+    bunny.y = 40 * j;
+    container.addChild(bunny);
+  };
+};
+
+container.x = 100;
+container.y = 60;
 
 // start animating
 animate();
@@ -13,26 +33,6 @@ animate();
 function animate() {
 
   requestAnimationFrame(animate);
-
-  // create the root of the scene graph
-  var stage = new PIXI.Container();
-
-  var container = new PIXI.Container();
-
-  stage.addChild(container);
-
-  for (var j = 0; j < 5; j++) {
-
-    for (var i = 0; i < 5; i++) {
-      var bunny = PIXI.Sprite.fromImage('_assets/basics/bunny.png');
-      bunny.x = 40 * i;
-      bunny.y = 40 * j;
-      container.addChild(bunny);
-    };
-  };
-
-  container.x = 100;
-  container.y = 60;
 
   // render the root container
   renderer.render(stage);
