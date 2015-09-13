a559d759d65a90bb07eca4ae42d226669751d0a1
arminhammer <arminhammer@gmail.com>
2015-09-13 17:47:21 -0400

    Pixijs now integrated into mithril.

--- app/pixi/pixi.script.js
+++ app/pixi/pixi.script.js
@@ -4,7 +4,7 @@
 
 var width = window.innerWidth;
 var height = window.innerHeight;
-var renderer = PIXI.autoDetectRenderer(width, height,{backgroundColor : 0xffffff});
+var renderer = PIXI.autoDetectRenderer(width, height,{backgroundColor : 0xffff00});
 document.body.appendChild(renderer.view);
 
 var elementSize = 100;
@@ -222,50 +222,6 @@ elements.forEach(function(element) {
   console.log(element.height + ':' + element.width);
   stage.addChild(element);
 });
-
-/*
-graphics.lineStyle(4, 0x000000, 1);
-graphics.moveTo(250,0);
-graphics.lineTo(250,height);
-
-graphics.lineStyle(1, 0x000000, 1);
-graphics.moveTo(270,0);
-graphics.lineTo(270,height);
-
-var graphics = new PIXI.Graphics();
-
-
-// set a fill and line style
-graphics.beginFill(0xFF3300);
-graphics.lineStyle(4, 0xffd900, 1);
-
-// draw a shape
-graphics.moveTo(50,50);
-graphics.lineTo(250, 50);
-graphics.lineTo(100, 100);
-graphics.lineTo(50, 50);
-graphics.endFill();
-
-// set a fill and a line style again and draw a rectangle
-graphics.lineStyle(2, 0x0000FF, 1);
-graphics.beginFill(0xFF700B, 1);
-graphics.drawRect(50, 250, 120, 120);
-
-// draw a rounded rectangle
-graphics.lineStyle(2, 0xFF00FF, 1);
-graphics.beginFill(0xFF00BB, 0.25);
-graphics.drawRoundedRect(150, 450, 300, 100, 15);
-graphics.endFill();
-
-// draw a circle, set the lineStyle to zero so the circle doesn't have an outline
-graphics.lineStyle(0);
-graphics.beginFill(0xFFFF0B, 0.5);
-graphics.drawCircle(470, 90,60);
-graphics.endFill();
-
-stage.addChild(graphics);
-
- */
 
 
 // start  animating
--- app/scripts/components/pixi.editor.js
+++ app/scripts/components/pixi.editor.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-function resizeGuiContainer() {
+function resizeGuiContainer(renderer) {
   var browserHeight = $('html').height();
   var browserWidth = $('html').width();
 
@@ -13,6 +13,11 @@ function resizeGuiContainer() {
 
   $('#guiContainer').height(browserHeight);
   $('#guiContainer').width(browserWidth);
+
+  /*if(renderer) {
+    renderer.view.style.width = browserWidth+'px';
+    renderer.view.style.height = browserHeight+'px';
+  }*/
 
   console.log('Resizing gui container...');
 
@@ -103,8 +108,187 @@ function drawSVG(paper, parsed, element, options) {
   }
 }
 
+
+function drawGrid(width, height) {
+  var grid = new PIXI.Graphics();
+  var interval = 25;
+  var count = interval;
+  grid.lineStyle(1, 0xE5E5E5, 1);
+  while (count < width) {
+    grid.moveTo(count, 0);
+    grid.lineTo(count, height);
+    count = count + interval;
+  }
+  var count = interval;
+  while(count < height) {
+    grid.moveTo(0, count);
+    grid.lineTo(width, count);
+    count = count + interval;
+  }
+  return grid;
+}
+
+function onDragStart(event) {
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  this.data = event.data;
+  this.alpha = 0.5;
+  this.dragging = true;
+}
+
+function onDragEnd() {
+  this.alpha = 1;
+
+  this.dragging = false;
+
+  // set the interaction data to null
+  this.data = null;
+}
+
+function onDragMove() {
+  if (this.dragging)
+  {
+    var global = this.toGlobal(this.parent);
+    var local = this.toLocal(this.parent);
+    console.log('x: ' + this.x + ' y: ' + this.y);
+    console.log('this: ' + this.x+":"+this.y + ", global: " + global.x + ":" + global.y + ", local: " + local.x + ":" + local.y);
+    //console.log('width: ' + this.width + ' height: ' + this.height);
+    var newPosition = this.data.getLocalPosition(this.parent);
+    console.log('NEW: ' + newPosition.x + ':' + newPosition.y);
+    var local = this.toLocal(this.data);
+    console.log('LOCAL: ' + local.x + ':' + local.y);
+    this.position.x = newPosition.x;
+    this.position.y = newPosition.y;
+    //this.moveTo(newPosition.x, newPosition.y);
+  }
+}
+
+function onMouseOver() {
+  var self = this;
+  var iconSize = 10;
+
+  console.log('Mouse over');
+  var global = self.toGlobal(self.parent);
+  console.log('GLOBAL: ' + global.x + ':' + global.y);
+
+  var scaleLocations = [
+    {x: -5, y: -5, size: iconSize},
+    {x: elementSize-5, y: -5, size: iconSize},
+    {x: -5, y: elementSize-5, size: iconSize},
+    {x: elementSize-5, y: elementSize-5, size: iconSize}
+  ];
+
+  //moveIcon.drawRect(elementSize-5, -5, 10, 10);
+  //moveIcon.drawRect(-5, elementSize-5, 10, 10);
+  //moveIcon.drawRect(elementSize-5, elementSize-5, 10, 10);
+
+  self.scaleIcons = [];
+
+  scaleLocations.forEach(function(loc) {
+    var icon = new PIXI.Graphics();
+    icon.interactive = true;
+    icon.buttonMode = true;
+    icon.lineStyle(1, 0x0000FF, 1);
+    icon.beginFill(0x000000, 1);
+    icon.drawRect(loc.x, loc.y, loc.size, loc.size);
+    icon.endFill();
+
+    icon
+      // events for drag start
+      .on('mousedown', onScaleIconDragStart)
+      .on('touchstart', onScaleIconDragStart);
+    // events for drag end
+    //.on('mouseup', onDragEnd)
+    //.on('mouseupoutside', onDragEnd)
+    //.on('touchend', onDragEnd)
+    //.on('touchendoutside', onDragEnd)
+    // events for drag move
+    //.on('mousemove', onDragMove)
+    //.on('touchmove', onDragMove)
+
+    self.scaleIcons.push(icon);
+
+  });
+
+  //moveIcon.endFill();
+
+  //stage.removeChild(self);
+  //stage.addChild(icon);
+  self.scaleIcons.forEach(function(s) {
+    self.addChild(s);
+  });
+  //this.addChild(this.scaleIcons[0])
+  //stage.addChild(this);
+
+}
+
+function onMouseOut() {
+  var self = this;
+  console.log('Mouse out');
+  this.scaleIcons.forEach(function(s) {
+    self.removeChild(s);
+  });
+  console.log('Size: ');
+  console.log(this.getBounds());
+
+}
+
+function onScaleIconDragStart(event) {
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  this.data = event.data;
+  this.alpha = 0.5;
+  this.dragging = true;
+  console.log('Resizing!');
+}
+
+/*
+var elements = [];
+var el1 = createElement('el1', 100, 100);
+var el2 = createElement('el2', 200, 200);
+var el3 = createElement('el3', 300, 300);
+elements.push(el1);
+elements.push(el2);
+elements.push(el3);
+
+elements.forEach(function(element) {
+  console.log(element.name);
+  console.log(element.position);
+  console.log(element.height + ':' + element.width);
+  stage.addChild(element);
+});
+*/
+
+function draw() {
+
+}
+
 var PixiEditor = {
   controller: function(options) {
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    var elementSize = 100;
+
+    var renderer = PIXI.autoDetectRenderer(width, height,{backgroundColor : 0xFFFFFF});
+
+    // create the root of the scene graph
+    var stage = new PIXI.Container();
+
+    stage.interactive = true;
+    var meter = new FPSMeter();
+
+    var grid = drawGrid(width, height);
+    stage.addChild(grid);
+
+    function animate() {
+      renderer.render(stage);
+      meter.tick();
+      requestAnimationFrame(animate);
+    }
 
     return {
       template: options.template,
@@ -112,16 +296,22 @@ var PixiEditor = {
       drawCanvasEditor: function (element, isInitialized, context) {
 
         if (isInitialized) {
+          animate();
           return;
         }
 
         $(window).resize(function() {
-          resizeGuiContainer();
+          resizeGuiContainer(renderer);
           //drawSVG(paper, parsed, element, options);
+
         });
 
         resizeGuiContainer();
         //drawSVG(paper, parsed, element, options);
+
+        element.appendChild(renderer.view);
+
+        animate();
 
       }
     }
