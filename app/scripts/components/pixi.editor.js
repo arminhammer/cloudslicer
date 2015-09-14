27403404ea95f190e2e3c1041f51f9450df09806
Armin Graf <arminhammer@gmail.com>
2015-09-14 18:17:20 -0400

    Working on getting the sprite sheet to be read in.

--- app/scripts/components/pixi.editor.js
+++ app/scripts/components/pixi.editor.js
@@ -27,92 +27,6 @@ function resizeGuiContainer(renderer) {
 
 }
 
-function drawSVG(paper, parsed, element, options) {
-
-  console.log('Drawing...');
-
-  try {
-    parsed = JSON.parse(options.template());
-  }
-  catch (e) {
-    console.log('Parse error: ' + e);
-    paper = Snap(element);
-    if (paper) {
-      console.log('Clearing bc of error...');
-      paper.clear();
-    }
-  }
-
-  console.log('parsed is ' + parsed);
-
-  console.log($(element));
-
-  if (parsed) {
-
-    paper = Snap(element);
-    paper.clear();
-
-    console.log(parsed.Resources);
-
-    var ec2instances = _.filter(parsed.Resources, function (resource) {
-      return resource.Type === 'AWS::EC2::Instance'
-    });
-
-    console.log(ec2instances);
-
-    var paperCoordinates = {
-      x: $(element).width(),
-      y: $(element).height()
-    };
-
-    console.log('Coordinates:');
-    console.log(paperCoordinates);
-
-    var rowCount = 0;
-
-    ec2instances.forEach(function (ec2, key, index) {
-
-      var xCount = key % 2;
-      if(xCount === 0) {
-        rowCount++;
-      }
-
-      console.log('Index:');
-      console.log('xCount=' + xCount + ' and rowCount=' + rowCount);
-      //console.log(paperCoordinates.x/(index.length+1));
-
-      var fragment = Snap.parse(ec2icon);
-      svgCache.ec2instances[key] = fragment.select('svg');
-      var xVal = (key + 1) * 100;
-
-      svgCache.ec2instances[key].attr({
-        x: ((paperCoordinates.x/(index.length+1)) * (xCount + 1)) - (parseInt(svgCache.ec2instances[key].attr('width'))/2),
-        y: (((paperCoordinates.y/(index.length+1)) * rowCount) - (parseInt(svgCache.ec2instances[key].attr('height'))/2)) - 50
-      });
-
-      //animation
-      function animateSVG(){
-        svgCache.ec2instances[key].animate({cy: 300}, 5000,mina.bounce);
-        svgCache.ec2instances[key].animate({fill:"red"},200);
-      }
-
-      //reset function?
-      function resetSVG(){
-        // something here to reset SVG??
-      }
-
-      svgCache.ec2instances[key].mouseover(animateSVG,resetSVG);
-
-      console.log('Drawing at x: ' + svgCache.ec2instances[key].attr('x') + ', y: ' + svgCache.ec2instances[key].attr('y'));
-      paper.append(svgCache.ec2instances[key]);
-      svgCache.ec2instances[key].drag();
-
-    });
-
-  }
-}
-
-
 function drawGrid(width, height) {
   var grid = new PIXI.Graphics();
   var interval = 25;
@@ -297,31 +211,36 @@ var PixiEditor = {
         //var width = window.innerWidth;
         //var height = window.innerHeight;
 
-        var elementSize = 100;
+        //var elementSize = 100;
 
         stage.interactive = true;
-        //console.log(stage);
         var grid = stage.addChild(drawGrid(winDimension.x, winDimension.y));
-        //console.log(stage);
 
-        $(window).resize(function() {
-          resizeGuiContainer(renderer);
-          winDimension = getWindowDimension();
-          //console.log(newDim);
-          console.log(stage);
-          stage.removeChild(grid);
-          grid = stage.addChild(drawGrid(winDimension.x, winDimension.y));
+        //resizeGuiContainer();
 
-          //drawSVG(paper, parsed, element, options);
+        PIXI.loader
+          .add('../resources/sprites/sprites.json')
+          .load(function() {
 
-        });
+            var EC2Instance = PIXI.Texture.fromFrame('Compute_&_Networking_Amazon_EC2--.png');
+            console.log('EC2Instance');
+            console.log(EC2Instance);
+            stage.addChild(EC2Instance);
 
-        resizeGuiContainer();
-        //drawSVG(paper, parsed, element, options);
+            element.appendChild(renderer.view);
 
-        element.appendChild(renderer.view);
+            $(window).resize(function() {
+              resizeGuiContainer(renderer);
+              winDimension = getWindowDimension();
+              //console.log(newDim);
+              console.log(stage);
+              stage.removeChild(grid);
+              grid = stage.addChild(drawGrid(winDimension.x, winDimension.y));
+            });
 
-        animate();
+            animate();
+
+          });
 
       }
     }
